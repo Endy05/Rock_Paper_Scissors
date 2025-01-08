3cfddbb039dfd9e75f547543d66d5d2c52ad7a88
ANDRII <119053818+Endy05@users.noreply.github.com>
2025-01-09 00:18:01 +0200

    add ui

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
+humanSelection = document.querySelector('.humanChoice');
+computerSelection = document.querySelector('.computerChoice');
+score = document.querySelector('.score');
+round = document.querySelector('.round');
+winner = document.querySelector(".winner");
+playAgain = document.querySelector(".game__box");
+
+const rock = document.querySelector('.rock');
+rock.addEventListener('click', () => {
+    playGame('rock');
+});
+
+const paper = document.querySelector('.paper');
+paper.addEventListener('click', () => {
+    playGame('paper');
+});
+
+const scissors = document.querySelector('.scissors');
+scissors.addEventListener('click', () => {
+    playGame('scissors');
+});
+
 humanScore = 0;
 computerScore = 0;
+let rounds = 0;
 
 const choices = ['rock', 'paper', 'scissors'];
 
-function getComputerChoice(coices = ['rock', 'paper', 'scissors']) {
+function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
 
-function getHumanChoice() {
-    const humanChoice = prompt('What is your choice?');
-    if (humanChoice === 'rock' || humanChoice === 'paper' || humanChoice === 'scissors') {
-        return humanChoice;
-    } else {
-        console.log('Invalid choice');
-        return getHumanChoice();
-    }
-}
-
 function playRound(humanChoice, computerChoice) {
+    rounds++;
+
     if (humanChoice === computerChoice) {
         return 'It is a tie!';
     }
@@ -33,38 +48,49 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
-function playGame() {
-    let rounds = 0;
+function playGame(humanChoice1) {
+    const humanChoice = humanChoice1;
+    const computerChoice = getComputerChoice();
 
-    while (rounds < 5) {
-        const humanChoice = getHumanChoice();
-        const computerChoice = getComputerChoice();
-        console.log(playRound(humanChoice, computerChoice));
-        console.log('Computer: ' + computerChoice);
-        console.log('Human: ' + humanChoice);
-        console.log('Human: ' + humanScore + ' Computer: ' + computerScore);
-        rounds++;
-    }
+    console.log(playRound(humanChoice, computerChoice));
 
-    if (humanScore > computerScore) {
-        console.log('You win the game!');
-    } else if (humanScore < computerScore) {
-        console.log('You lose the game!');
-    } else {
-        console.log('It is a tie!');
+    updateDashbord(rounds, humanChoice, computerChoice);
+
+    if (computerScore >= 5 || humanScore >= 5) {
+        if (humanScore > computerScore) {
+            winner.textContent = "YOU WIN :)";
+        } else {
+            winner.textContent = "YOU LOSE :(";
+        }
+
+        document.querySelector('.rock').style.display = 'none';
+        document.querySelector('.paper').style.display = 'none';
+        document.querySelector('.scissors').style.display = 'none';
+
+        const button = document.createElement('button');
+        button.textContent = "Restart Game";
+        button.classList.add('restart');
+        document.querySelector('.game__box').appendChild(button);
+
+        button.addEventListener('click', () => {
+            document.querySelector('.rock').style.display = 'inline-block';
+            document.querySelector('.paper').style.display = 'inline-block';
+            document.querySelector('.scissors').style.display = 'inline-block';
+
+            winner.textContent = '';
+
+            humanScore = 0;
+            computerScore = 0;
+            rounds = 0;
+            updateDashbord(rounds, 'null', 'null');
+            button.remove();
+        });
     }
 }
 
-playGame();
-
-
-
-
-
-
-
-
-
-
-
-
+function updateDashbord(rounds, humanChoice, computerChoice) {
+    humanSelection.textContent = humanChoice;
+    computerSelection.textContent = computerChoice;
+    score.textContent = humanScore + " : " + computerScore;
+    round.textContent = rounds;
+}
